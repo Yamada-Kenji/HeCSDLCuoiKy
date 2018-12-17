a645c0a5435224f3364147bc51194df814a34841
YukiHai <ngochai1591@gmail.com>
2018-12-17 13:34:49 +0700

    Revert "update"
    This reverts commit c5ece6f1590bfc01f8b904539195177f7573ec64.

--- DoAnHCSDL/SQLCOMMAND.sql
+++ DoAnHCSDL/SQLCOMMAND.sql
@@ -5,18 +5,14 @@ go
 
 execute DangNhap YukiHai , asdasd123
 go 
-alter proc ThemMon @MaMon nvarchar(50) ,@TenMon nvarchar(50) , @DanhMuc nvarchar(50), @GiaTien int, @HinhAnh image , @MoTa nvarchar(1000)
+create proc ThemMon @TenMon nvarchar(50) , @DanhMuc nvarchar(50), @GiaTien int, @HinhAnh image , @MoTa nvarchar(1000)
 as
-insert into Menu values (@MaMon,@TenMon,@DanhMuc,@GiaTien,@HinhAnh,@MoTa)
+insert into Menu values (@TenMon,@DanhMuc,@GiaTien,@HinhAnh,@MoTa)
 go
 
-alter proc LayTatCaDanhMuc 
+create proc LayTatCaDanhMuc 
 as
 select distinct DanhMuc from Menu 
 go
 
-alter proc LayThongTinMonAn @DanhMuc nvarchar(50)
-as
-select * from Menu where DanhMuc like @DanhMuc
-go
-select * from Menu
+select * from Menu where DanhMuc = 'Coffe'
